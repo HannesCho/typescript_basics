8910a76da0a1a0496421a827463d8443a040ede9
HannesCho <63042736+HannesCho@users.noreply.github.com>
2021-10-11 23:29:25 +0200

    add interface

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
-};
-console.log(sayHi("Nicolas", 24, "male"));
-1;
-//# sourceMappingURL=index.js.map
