d17238b8fa3b2bc20157977a38ef7e66499a3cd0
HannesCho <63042736+HannesCho@users.noreply.github.com>
2021-10-11 23:07:04 +0200

    add function

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-alert("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+};
+console.log(sayHi("Nicolas", 24, "male"));
+1;
 //# sourceMappingURL=index.js.map
