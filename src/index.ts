75ed3c00b69f22bfd960a1bc179cf5e9f13cdb83
HannesCho <63042736+HannesCho@users.noreply.github.com>
2021-10-12 20:31:55 +0200

    add block

--- src/index.ts
+++ src/index.ts
@@ -10,23 +10,51 @@
 //   gender: "male"
 // };
 
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-  }
+// class Human {
+//   public name: string;
+//   public age: number;
+//   public gender: string;
+//   constructor(name: string, age: number, gender: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+//   }
+// }
 
-  const lynn = new Human("Lynn", 18, "female");
-const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-  };
+//   const lynn = new Human("Lynn", 18, "female");
+// const sayHi = (person: Human): string => {
+//     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+//   };
   
-  // console.log(sayHi(person));
+//   // console.log(sayHi(person));
   
-  console.log(sayHi(lynn));
+//   console.log(sayHi(lynn));
+
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
+}
+
+const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
+
+let blockchain: [Block] = [genesisBlock];
+
+console.log(blockchain);
 
   export {};
